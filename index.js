72d35f8b14d1f5c41e93ab4b87074aa59064974b
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-03 23:03:42 +0500

    Handle HTTP requests and respond with corresponding web page

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const https = require("https");
+const http = require("http");
 const url = require("url");
 const fs = require("fs/promises");
 
-const server = https.createServer();
+const server = http.createServer();
 
-server.on("request", (req, res) => {
+server.on("request", async (req, res) => {
+  const parsed_url = url.parse(req.url, true);
+  let filename = "./" + parsed_url.pathname;
 
+  if ((filename = "./")) {
+    filename = "./index.html";
+  }
+
+  try {
+    const data = await fs.readFile(filename, { encoding: "utf-8" });
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+  } catch (e) {
+    const data = await fs.readFile("./404.html", { encoding: "utf-8" });
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write(data);
+  }
+
+  return res.end();
 });
 
 server.listen(8080);
